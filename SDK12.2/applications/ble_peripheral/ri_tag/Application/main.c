9de25d741a6a68e4a7dfe1e2cf80b632d21e532c
Jeffrey Wedekind <jeffreywedekind@gmail.com>
2017-02-11 01:53:26 -0600

    minimum project compiled

---
+++ SDK12.2/applications/ble_peripheral/ri_tag/Application/main.c
@@ -0,0 +1,33 @@
+/*
+ *
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "nordic_common.h"
+
+#if 0
+#include "app_error.h"
+#include "app_uart.h"
+#include "ble_db_discovery.h"
+#include "app_timer.h"
+#include "app_util.h"
+#include "bsp.h"
+#include "bsp_btn_ble.h"
+#include "boards.h"
+#include "ble.h"
+#include "ble_gap.h"
+#include "ble_hci.h"
+#include "softdevice_handler.h"
+#include "ble_advdata.h"
+#include "ble_nus_c.h"
+#endif
+
+int main(void)
+{
+	while (1)
+    {
+    }
+}
